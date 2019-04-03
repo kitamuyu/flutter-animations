72426c6b6a6f370b2b08eab1abae3ada9c797676
Yu Kitamura <kitamura.ky.monkey@gmail.com>
2019-04-03 21:47:56 +0900

    Animate menu

--- lib/Animations/CustomMenuAnimation.dart
+++ lib/Animations/CustomMenuAnimation.dart
@@ -8,29 +8,7 @@ class CustomMenuPage extends StatefulWidget {
   _MenuState createState() => _MenuState();
 }
 
-class _MenuState extends State<CustomMenuPage>
-    with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    );
-    _controller.addListener(() {
-      setState(() {});
-    });
-    _controller.forward().orCancel;
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _MenuState extends State<CustomMenuPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -64,83 +42,13 @@ class Guillotine extends StatefulWidget {
   _GuillotineState createState() => _GuillotineState();
 }
 
-class _GuillotineState extends State<Guillotine> {
-  double rotationAngle = 0;
+class _GuillotineState extends State<Guillotine>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> animationMenu;
+  Animation<double> animationTitleInOut;
+  _GuillotineAnimationStatus menuAnimationStatus;
 
-  @override
-  Widget build(BuildContext context) {
-    MediaQueryData mediaQueryData = MediaQuery.of(context);
-    double screenWidth = mediaQueryData.size.width;
-    double screenHeight = mediaQueryData.size.height;
-
-    return Transform.rotate(
-      angle: rotationAngle,
-      origin: Offset(24, 56),
-      alignment: Alignment.topLeft,
-      child: Material(
-        color: Colors.transparent,
-        child: Container(
-          width: screenWidth,
-          height: screenHeight,
-          color: Color(0xFF333333),
-          child: Stack(
-            children: <Widget>[
-              _buildMenuTitle(),
-              _buildMenuIcon(),
-              _buildMenuContent(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMenuTitle() {
-    return Positioned(
-      top: 32,
-      left: 40,
-      width: MediaQuery.of(context).size.width, //screenWidth?
-      height: 24,
-      child: Transform.rotate(
-        alignment: Alignment.topLeft,
-        origin: Offset.zero,
-        angle: pi / 2,
-        child: Center(
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Opacity(
-              opacity: 1,
-              child: Text(
-                'warmful',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2,
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-Widget _buildMenuIcon() {
-  return Positioned(
-    top: 32,
-    left: 4,
-    child: IconButton(
-      icon: const Icon(Icons.menu, color: Colors.white),
-      onPressed: () {},
-    ),
-  );
-}
-
-Widget _buildMenuContent() {
   final List<Map> _menus = <Map>[
     {
       "icon": Icons.person,
@@ -164,28 +72,151 @@ Widget _buildMenuContent() {
     },
   ];
 
-  return Padding(
-    padding: const EdgeInsets.only(left: 64, top: 96),
-    child: Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: _menus.map((menuItem) {
-            return ListTile(
-              leading: Icon(
-                menuItem["icon"],
-                color: menuItem["color"],
-              ),
-              title: Text(
-                menuItem["title"],
-                style: TextStyle(
-                  color: menuItem["color"],
-                  fontSize: 24,
+  @override
+  void initState() {
+    super.initState();
+    menuAnimationStatus = _GuillotineAnimationStatus.closed;
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    _controller.addListener(() {});
+
+    animationMenu = Tween(
+      begin: -pi / 2,
+      end: 0,
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Curves.bounceOut,
+        reverseCurve: Curves.bounceIn))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((AnimationStatus status) {
+        switch (status) {
+          case AnimationStatus.completed:
+            menuAnimationStatus = _GuillotineAnimationStatus.open;
+            break;
+          case AnimationStatus.dismissed:
+            menuAnimationStatus = _GuillotineAnimationStatus.closed;
+            break;
+          default:
+            menuAnimationStatus = _GuillotineAnimationStatus.animating;
+        }
+      });
+
+    animationTitleInOut = Tween(begin: 1.0, end: 0.0).animate(CurvedAnimation(
+        parent: _controller, curve: Interval(0, 0.5, curve: Curves.ease)));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _playAnimation() {
+    try {
+      switch (menuAnimationStatus) {
+        case _GuillotineAnimationStatus.animating:
+          break;
+        case _GuillotineAnimationStatus.closed:
+          _controller.forward().orCancel;
+          break;
+        default:
+          _controller.reverse().orCancel;
+      }
+    } on TickerCanceled {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    double angle = animationMenu.value;
+
+    return Transform.rotate(
+      angle: angle,
+      origin: Offset(24, 56),
+      alignment: Alignment.topLeft,
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          width: screenWidth,
+          height: screenHeight,
+          color: Color(0xFF333333),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 32,
+                left: 40,
+                width: screenWidth,
+                height: 24,
+                child: Transform.rotate(
+                  alignment: Alignment.topLeft,
+                  origin: Offset.zero,
+                  angle: pi / 2,
+                  child: Center(
+                    child: Container(
+                      width: double.infinity,
+                      height: double.infinity,
+                      child: Opacity(
+                        opacity: animationTitleInOut.value,
+                        child: Text(
+                          'warmful',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            );
-          }).toList()),
-    ),
-  );
+              Positioned(
+                top: 32,
+                left: 4,
+                child: IconButton(
+                  icon: const Icon(Icons.menu, color: Colors.white),
+                  onPressed: _playAnimation,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 64, top: 96),
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: _menus.map((menuItem) {
+                        return ListTile(
+                          leading: Icon(
+                            menuItem["icon"],
+                            color: menuItem["color"],
+                          ),
+                          title: Text(
+                            menuItem["title"],
+                            style: TextStyle(
+                              color: menuItem["color"],
+                              fontSize: 24,
+                            ),
+                          ),
+                        );
+                      }).toList()),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
+
+enum _GuillotineAnimationStatus { closed, open, animating }
