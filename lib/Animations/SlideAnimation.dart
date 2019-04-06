5ed789169f7d6efdc659055114fb3579130c7f2f
Yu Kitamura <kitamura.ky.monkey@gmail.com>
2019-04-06 16:26:23 +0900

    Add file

---
+++ lib/Animations/SlideAnimation.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SlideAnimationPage extends StatefulWidget {
+  @override
+  SlideCards createState() => SlideCards();
+}
+
+class SlideCards extends State<SlideAnimationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
